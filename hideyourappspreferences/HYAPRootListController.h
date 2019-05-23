a2fcae5f6bd29bac373a644a5962f37fea0ba4b0
Blake Boxberger <rockiesrockyoursocks@gmail.com>
2019-05-22 17:01:29 -0700

    Fixed bugs

--- hideyourappspreferences/HYAPRootListController.h
+++ hideyourappspreferences/HYAPRootListController.h
@@ -1,5 +1,7 @@
 #import <Preferences/PSListController.h>
 
 @interface HYAPRootListController : PSListController
-
+- (void)openUrl:(PSSpecifier *)specifier;
+- (void)respring:(PSSpecifier *)specifier;
+- (void)reset:(PSSpecifier *)specifier;
 @end
